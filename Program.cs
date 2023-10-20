fb846438e1b643db25bea3b22af801e507d711b8
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-20 15:30:47 +0200

    Identiy scaffolding added

--- Program.cs
+++ Program.cs
@@ -2,78 +2,93 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Quickfire_Bulletin_ASP.NET.Areas.Identity.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Make sure this connection string is included in your appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("Quickfire_Bulletin_ContextConnection") ?? throw new InvalidOperationException("Connection string 'Quickfire_Bulletin_ContextConnection' not found.");
 
+// Register database context and identity
 builder.Services.AddDbContext<Quickfire_Bulletin_Context>(options =>
     options.UseSqlServer(connectionString));
 
-// Add Identity configuration
-builder.Services.AddDefaultIdentity<Quickfire_Bulletin_ASPNETUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 4;
-    options.Password.RequiredUniqueChars = 0;
-})
+builder.Services.AddDefaultIdentity<Quickfire_Bulletin_ASPNETUser>()
+    .AddRoles<IdentityRole>() // Include Role services
     .AddEntityFrameworkStores<Quickfire_Bulletin_Context>();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();  // Added RazorPages to services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+await Task.Run(() => ConfigureApp(app));
+
+async Task ConfigureApp(WebApplication app)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Identity Role and User creation
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var roles = new[] { "Admin", "Member" };
-    foreach (var role in roles)
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
     {
-        if (!await roleManager.RoleExistsAsync(role))
-            await roleManager.CreateAsync(new IdentityRole(role));
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
     }
-}
 
-using (var scope = app.Services.CreateScope())
-{
-    var _userManager = scope.ServiceProvider.GetService<UserManager<Quickfire_Bulletin_ASPNETUser>>();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-    string email = "admin@admin.com";
-    string password = "1234";
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-    if (await _userManager.FindByEmailAsync(email) == null)
+    app.UseEndpoints(endpoints =>
     {
-        var user = new Quickfire_Bulletin_ASPNETUser
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapRazorPages();  // Enclosed in UseEndpoints
+    });
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var roles = new[] { "Admin", "Member" };
+        foreach (var role in roles)
         {
-            UserName = email,
-            Email = email,
-        };
-        await _userManager.CreateAsync(user, password);
-        await _userManager.AddToRoleAsync(user, "Admin");
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
     }
-}
 
-app.Run();
+    using (var scope = app.Services.CreateScope())
+    {
+        var _userManager = scope.ServiceProvider.GetService<UserManager<Quickfire_Bulletin_ASPNETUser>>();
+
+        string email = "admin@admin.com";
+        string password = "1234";
+
+        if (await _userManager.FindByEmailAsync(email) == null)
+        {
+            var user = new Quickfire_Bulletin_ASPNETUser
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine($"Error: {error.Description}");
+                }
+            }
+        }
+    }
+
+    app.Run();
+}
